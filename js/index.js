0a3546e5370fdd2d88ec648d3f42da826dafc4a9
angeliaz <angeliaw@yeah.net>
2015-08-10 19:42:19 +0800

    add the setInterval timer

--- js/index.js
+++ js/index.js
@@ -12,14 +12,15 @@
 
 	var defaultSettings = {
 		initSnakeNum: 4,
+		score: 10,
 		row: 20,
         column: 30,
         cellWidth: 30,
         cellHeight: 30,
         bgStrokeStyle: '#f2dede',
-        bgFillStyle: '#d9edf7',
+        bgFillStyle: '#fff', //'#d9edf7',
         foodStyle: '#a94442',
-        snakeStyle: '#3c763d'
+        snakeStyle: '#31708f'
 	};
 
 	function Snake (settings) {
@@ -28,7 +29,7 @@
 			this[key] = settings[key] ? settings[key] : defaultSettings[key];
 		}
 
-		this.points = [];
+		this.lock = true;
 		this.currentDirection = 'down';
 		this.access = {
 			'up': 'down',
@@ -47,9 +48,10 @@
 			this.canvas.width = this.cellWidth * this.column;
 			this.canvas.height = this.cellHeight * this.row;
 			this.drawBg();
-			this.drawFood();
 			this.drawSnake();
+			this.drawFood();
 			this.bindEvent();
+			this.autoMove();
 
 		},
 
@@ -64,20 +66,30 @@
 			}
 		},
 
-		// 画食物： 位置随机、实心矩形
+		// 画食物： 位置随机、实心矩形、不能在蛇身上
 		drawFood: function () {
 			
+			var foodLock = true;
 			this.foodX = Math.floor(Math.random() * this.column) || 0;
 			this.foodY = Math.floor(Math.random() * this.row) || 0;
-			console.log(this.foodX + '; ' + this.foodY);
+
+			// TODO： 排除食物画在蛇身上的情况
+			for(var i = 0; i < this.points.length; i++) {
+				if (this.points[i].x === this.foodX && this.points[i].y === this.foodY) {
+					this.drawFood();
+					foodLock = false;
+					break;
+				}
+			}
+
 			this.ctx.save();
 			this.ctx.fillStyle = this.foodStyle;
-			this.ctx.fillRect(this.foodX * this.cellWidth, this.foodY * this.cellHeight, this.cellWidth, this.cellHeight);
-			// TODO： 排除食物画在蛇身上的情况
+			this.ctx.fillRect(this.foodX * this.cellWidth + 1, this.foodY * this.cellHeight + 1, this.cellWidth - 2 , this.cellHeight - 2);
 		},
 
-		// 画蛇
+		// 初始化蛇
 		drawSnake: function () {
+			this.points = [];
 			this.ctx.save();
 			this.ctx.fillStyle = this.snakeStyle;
 			for(var i = 0; i < this.initSnakeNum; i++) {
@@ -86,18 +98,19 @@
 			}
 		},
 
+		autoMove: function () {
+			var _this = this;
+			this.timer = setInterval(function () {
+				_this.move();
+			}, 1000);
+		},
+
 		// 移动: 如果没有吃到食物，每次移动： 蛇头前新增一个，去掉蛇尾
 		// 如果吃到食物，将食物颜色变成蛇的颜色（算的块数+1）
 		move: function () {
-			// if (this.access[this.currentDirection] !== this.oldDirection) {
 				
-				this.addHead(this.points[this.points.length - 1], this.currentDirection);
-				this.deleteTail(this.points[0]);
-				this.oldDirection = this.currentDirection;
-			// }
-			// if (this.currentDirection === 'down' && this.oldDirection !== 'up') {
-				
-			// }
+			this.addHead(this.points[this.points.length - 1], this.currentDirection);
+			this.oldDirection = this.currentDirection;
 
 		},
 
@@ -128,35 +141,75 @@
 				addY = points.y;
 				break;
 			}
+			if (addX < 0 || addX >= this.column || addY < 0 || addY >= this.row) {
+				this.lock = false;
+				clearInterval(this.timer);
+				alert('亲，您撞墙了哦，重新开始吧~');
+			} else if (addX === this.foodX && addY === this.foodY ) {
+				this.ctx.clearRect(addX * this.cellWidth + 1, addY * this.cellHeight + 1, this.cellWidth - 2, this.cellHeight - 2);
+				this.ctx.fillRect(addX * this.cellWidth + 1, this.cellHeight * addY + 1, this.cellWidth - 2, this.cellHeight - 2);
+				this.points.push({x:addX , y:addY});
+				this.drawFood(); // 吃完食物后生成下一个食物
+				this.getScore();
+			} else {
+				for (var i = 0; i < this.points.length; i++) {
+					if (this.points[i].x === addX && this.points[i].y === addY) {
+						this.lock = false;
+						clearInterval(this.timer);
+						alert('亲，您干嘛要把自己吃掉呀，重新开始吧~');
+						return;
+					}
+				}
+				this.ctx.fillStyle = this.snakeStyle;
+				this.ctx.fillRect(addX * this.cellWidth + 1, this.cellHeight * addY + 1, this.cellWidth - 2, this.cellHeight - 2);
+				this.points.push({x:addX , y:addY});
+				this.deleteTail(this.points[0]);
+			}
+			
+		},
 
-			this.ctx.fillStyle = this.snakeStyle;
-			this.ctx.fillRect(addX * this.cellWidth + 1, this.cellHeight * addY + 1, this.cellWidth - 2, this.cellHeight - 2);
-			this.points.push({x:addX , y:addY});
+		getScore: function () {
+			$('#score').html(parseInt($('#score').html()) + this.score);
 		},
 
 		// 37：左, 38:上, 39:右, 40下
 		bindEvent: function () {
 			var _this = this;
+
+			$('#begin').on('click', function () {
+				if (!this.lock) {
+					$('#score').html('0');
+					_this.lock = true;
+					_this.ctx.restore();
+					_this.ctx.clearRect(0, 0, _this.column * _this.cellWidth, _this.row * _this.cellHeight);
+					_this.drawBg();
+					_this.drawSnake();
+					_this.drawFood();
+					_this.autoMove();
+				}
+			});
+
 			$(doc).on('keydown', function (e) {
 				
-				switch(e.keyCode) {
-				case 37: 
-					_this.currentDirection = 'left';
-					break;
-				case 38:
-					_this.currentDirection = 'up';
-					break;
-				case 39:
-					_this.currentDirection = 'right';
-					break;
-				case 40:
-					_this.currentDirection = 'down';
-					break;
+				if (_this.lock) {
+					switch(e.keyCode) {
+					case 37: 
+						_this.currentDirection = 'left';
+						break;
+					case 38:
+						_this.currentDirection = 'up';
+						break;
+					case 39:
+						_this.currentDirection = 'right';
+						break;
+					case 40:
+						_this.currentDirection = 'down';
+						break;
+					}
+					if (_this.access[_this.currentDirection] !== _this.oldDirection) { 
+						_this.move();
+					}
 				}
-				if (_this.access[_this.currentDirection] !== _this.oldDirection) { 
-					_this.move();
-				}
-					
 			});
 		}
 
